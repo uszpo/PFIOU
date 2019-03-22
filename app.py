580e8b2da56f389b4ad7aaafe089f0e4abe73d51
uszpo <maxime.u@gmail.com>
2019-03-22 13:12:30 +0100

    :)

--- app.py
+++ app.py
@@ -1,27 +1,16 @@
 from flask import request, Flask, session, render_template
-from flask_restful import Resource, Api
-from flask import jsonify
 import random
-
 app = Flask(__name__)
-api = Api(app)
 
 
-class Test(Resource):
-    def get(self, valeur_test):
-        if int(valeur_test) == session.get('aleatoire'):
-            return True
-        else:
-            return False
-
-@app.route('/')
-def get():
+@app.route('/', methods=['GET', 'POST'])
+def hello(valeur_test = None):
     if request.method == 'GET':
         alea = random.randint(1, 100)
         session['aleatoire'] = alea
         return render_template("hello.html")
     else:
-        pass # TODO http://flask.pocoo.org/docs/1.0/quickstart/#rendering-templates
+        return render_template("test.html") # TODO http://flask.pocoo.org/docs/1.0/quickstart/#rendering-templates
 
 
 app.config['SECRET_KEY'] = "Your_secret_string"
